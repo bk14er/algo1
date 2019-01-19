a7d4711765ece97e217da8f0f44cde63b8f61b16
bk14er <mateduraj@gmail.com>
2019-01-19 11:10:51 +0100

    passing parameters

--- src/main/java/pl/sda/structure/oop/problem2/PassingParameters.java
+++ src/main/java/pl/sda/structure/oop/problem2/PassingParameters.java
@@ -4,16 +4,31 @@ import java.math.BigDecimal;
 
 public class PassingParameters {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
+
+
+        StringBuilder stringBuilder = new StringBuilder()
+                .append("cos")
+                .append("test")
+                .append("fdsfd");
+
+        String builded = stringBuilder.toString();
+
+        BigDecimal.valueOf(12.3)
+                .add(new BigDecimal("123.2"))
+                .add(new BigDecimal("22.2"));
+
 
         Parameter parameter = new Parameter()
                 .sum(12)
-                .name("cos");
+                .amount(new BigDecimal("123,.12"))
+                .title("title")
+                .name("name");
 
     }
 
 
-    public static class Parameter{
+    public static class Parameter {
 
         private int sum;
         private String name;
@@ -23,24 +38,33 @@ public class PassingParameters {
         private String surname;
         private String address;
 
-        public Parameter sum(int sum){
+        public Parameter sum(int sum) {
             this.sum = sum;
             return this;
         }
 
-        public Parameter name(String name){
+        public Parameter name(String name) {
             this.name = name;
             return this;
         }
 
 
+        public Parameter title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Parameter amount(BigDecimal amount) {
+            this.amount = amount;
+            return this;
+        }
     }
 
 
     /**
      * Jak zastąpić wywołanie z wieloma parametrami ?
      */
-    public String execute(Parameter parameter){
+    public String execute(Parameter parameter) {
 
         // ..jakieś operacje...
         return "";
