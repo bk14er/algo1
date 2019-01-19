ef2cb2b44e9fa5aa0d93757c766dd90fcfae4ace
bk14er <mateduraj@gmail.com>
2019-01-19 10:55:26 +0100

    fix engine runner

--- src/main/java/pl/sda/structure/oop/problem2/PassingParameters.java
+++ src/main/java/pl/sda/structure/oop/problem2/PassingParameters.java
@@ -4,16 +4,45 @@ import java.math.BigDecimal;
 
 public class PassingParameters {
 
+    public static void main(String args[]){
+
+        Parameter parameter = new Parameter()
+                .sum(12)
+                .name("cos");
+
+    }
+
+
+    public static class Parameter{
+
+        private int sum;
+        private String name;
+        private String title;
+        private BigDecimal amount;
+        private BigDecimal credit;
+        private String surname;
+        private String address;
+
+        public Parameter sum(int sum){
+            this.sum = sum;
+            return this;
+        }
+
+        public Parameter name(String name){
+            this.name = name;
+            return this;
+        }
+
+
+    }
+
 
     /**
      * Jak zastąpić wywołanie z wieloma parametrami ?
      */
-    public String execute(int sum, String name, String title,
-                          BigDecimal amount, BigDecimal credit,
-                          String surname, String address){
+    public String execute(Parameter parameter){
 
         // ..jakieś operacje...
         return "";
     }
-
 }
