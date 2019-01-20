f9d6c7f64d7792fe6f33197ce2f3998e4c16674b
bk14er <mateduraj@gmail.com>
2019-01-20 13:58:47 +0100

    Lambda create

--- src/main/java/pl/sda/structure/streams/LambdaIntroduction.java
+++ src/main/java/pl/sda/structure/streams/LambdaIntroduction.java
@@ -1,9 +1,29 @@
 package pl.sda.structure.streams;
 
+import java.util.function.Predicate;
+
 public class LambdaIntroduction {
+
+
+    @FunctionalInterface
+    public interface FilterString {
+
+        boolean filterString(String str);
+
+    }
 
     public static void main(String args[]) {
 
+        Predicate<String> filter = str -> str.length() > 10;
+
+        System.out.println(filter.test("aabc"));
+
+//        (parameters) -> {ciało_funkcji}
+
+        FilterString filterAsLamdas = (String test) -> test.length() > 10;
+
+
+        System.out.println(filterAsLamdas.filterString("abds"));
 
     }
 
