9b0e5e3852494eb7b7cfb04ae6043bb0d63cf581
bk14er <mateduraj@gmail.com>
2019-01-26 14:00:18 +0100

    palindrome detector

--- src/main/java/pl/sda/algorithm/common/advanced/PalindromeDetector.java
+++ src/main/java/pl/sda/algorithm/common/advanced/PalindromeDetector.java
@@ -3,10 +3,20 @@ package pl.sda.algorithm.common.advanced;
 public class PalindromeDetector {
 
     public boolean isPalindrome(String text) {
+        text = text.replaceAll("!","");
 
         // Pierwsza i ostatnia litera muszą pasować , druga litera i przedostatnia itd...
         char[] content = text.toCharArray();
         int length = content.length;
+
+        for(int i=0 ; i < length; i++){
+
+            if(content[i] != content[length- 1 - i]){
+                return false;
+            }
+
+        }
+
 
         return true;
     }
