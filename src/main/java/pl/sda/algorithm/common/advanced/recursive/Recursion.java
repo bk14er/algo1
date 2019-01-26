2a07448a63e86950ca4a2f2cd4cc87780aeb3be4
bk14er <mateduraj@gmail.com>
2019-01-26 09:24:07 +0100

    silnia zadanie

--- src/main/java/pl/sda/algorithm/common/advanced/recursive/Recursion.java
+++ src/main/java/pl/sda/algorithm/common/advanced/recursive/Recursion.java
@@ -7,26 +7,62 @@ public class Recursion {
     public static void main(String args[]) {
 
         int n = 5;
+
+        System.out.println("Suma iteracyjnie: " + iterationSum(N));
+
+        System.out.println("Suma rekurencyjnie: " + recursionSum(N));
+
+        System.out.println("Silnia 5: " + fractional(N));
+
         System.out.println("REKURENCJA HEAD:");
         head(10);
 
         System.out.println("REKURENCJA TAIL:");
         tail(10);
 
-        System.out.println("Suma iteracyjnie: " + iterationSum(N));
-        System.out.println("Suma rekurencyjnie: " + recursionSum(N));
-
         System.out.println("Suma rekurenyjnie ze schowkiem: " + recursionSumWithAccumulator(1, N));
 
     }
 
     public static int iterationSum(int N) {
+        int result = 0;
+
+        for(int i= 1; i < N; i++){
+            result = i+result;
+        }
+
+        return result;
+    }
+
+    /**
+     * N
+     * 5 + recursionSum(4)
+     * 4 + recursionSum(3)
+     * 3 + recursionSum(2)
+     * 2 + recursionSum(1)
+     * 1  => 1
+     *
+     */
+    public static int recursionSum(int N) {
+        if(N ==1){
+            return 1;
+        }
+        return N + recursionSum(N-1);
+    }
+
+    /**
+    5! = 1 * 2 * 3 * 4 * 5
+
+    5 * silna(4) ..
+    4 * silnia(3)
+
+
+     */
+    public static int fractional(int N){
+
         return -1;
     }
 
-    public static int recursionSum(int N) {
-        return -1;
-    }
 
     public static int recursionSumWithAccumulator(int schowek, int N) {
         return -1;
