763af41371eacf66ff4d2c81512103e9be3bb11c
bk14er <mateduraj@gmail.com>
2019-01-26 15:04:27 +0100

    unique character detector

--- src/main/java/pl/sda/algorithm/common/advanced/UniqueCharacterDetector.java
+++ src/main/java/pl/sda/algorithm/common/advanced/UniqueCharacterDetector.java
@@ -8,6 +8,16 @@ public class UniqueCharacterDetector {
         //Pobranie numeru w tablicy znaków ASCI
         char asciChar = text.charAt(0);
         int asciCharValue = text.charAt(0);
+        char[] chars = text.toCharArray();
+
+        for(int i = 0 ; i < text.length(); i++){
+            int asci_value = text.charAt(i);
+            if(char_set[asci_value]){
+                return false;
+            }
+            char_set[asci_value] = true;
+        }
+
 
         return true;
     }
